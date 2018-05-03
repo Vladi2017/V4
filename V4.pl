2fb20bc9e2fa1b73136afca2d9dc6ce32ddfa01a
Vladimir Manolescu <mvmanol@yahoo.com>
2018-05-03 22:25:58 +0300

    rev.3.. OK
    batwings@VladiLaptopWXP /home/batwings/projects/perl/V4_DXTtelnet
    $ perl -X ./V4.pl dxt1
    ..started ./V4.pl rev.3, 10:20 PM Thursday, May 03, 2018
    started ./V4.pl rev.3, 10:20 PM Thursday, May 03, 2018
    manual_stty: 0
    dtcbIdx array: 66 58 38 39
    dtcbIdxAlias array: 66 DOSCINA 58 MESTECANIS 38 CARLIBABA 39 BOTOS
    iws array: TDSC-1-3 TDSC-30-0 TTRX-58-0 TBC-58-0 TBC-66-0 TTRX-66-0 TTRX-66-1 TDSC-3-0 TDSC-1-0 TTRX-92-0 TTRX-92-1
    
    batwings@VladiLaptopWXP /home/batwings/projects/perl/V4_DXTtelnet
    $ perl -X ./V4.pl dxt2
    ..started ./V4.pl rev.3, 10:20 PM Thursday, May 03, 2018
    started ./V4.pl rev.3, 10:20 PM Thursday, May 03, 2018
    manual_stty: 0
    dtcbIdx array: 66 35 84
    dtcbIdxAlias array: 66 NADRAG 35 ROSIUTA 84 TOROIOAGA
    iws array: TBC-66-0 TTRX-13-0 TTRX-13-1 CLS-0 TDSC-72-0 TDSC-75-0 TBC-84-0 TTRX-84-0 TDSC-1-0 TDSC-62-0
    
    batwings@VladiLaptopWXP /home/batwings/projects/perl/V4_DXTtelnet
    $
    
    Vladi_notes:
    hash of arrays
    @{$aref} //Vl. where $aref is an array reference..

--- V4.pl
+++ V4.pl
@@ -1,13 +1,15 @@
 #!/usr/bin/env perl
 ##Vl.8:30 PM Wednesday, May 02, 2018: $ perl -c V4.pl; $ perl ./V4.pl; RO2 (elements) recovering code..
 use strict;
+# no strict "refs";
 use warnings;
-use constant REV => "2, 5:27 PM Thursday, May 03, 2018";
+use constant REV => "3, 10:20 PM Thursday, May 03, 2018";
 use Expect;
 use v5.14;
 my @dxt1 = ("10.1.153.4", 5001); #Vl.work both "5001" and (int) 5001
 my @dxt2 = ("10.1.153.4", 5002);
-undef my @dxt;
+my %dxt = (1 => \@dxt1, 2 => \@dxt2); #Vl.hash of arrays
+undef my $dxtNum;
 undef my $monitoring;
 my $Usage = "Usage:\n$0 [--help]\n$0 dxt1|dxt2\n$0 --monitoringRO2\n";
 # say "nr params: ", $#ARGV + 1, " @ARGV";
@@ -15,14 +17,18 @@ say "..started $0 rev.", REV;
 die $Usage if (not @ARGV or @ARGV > 1);
 for ($ARGV[$#ARGV]){
   die $Usage 		when $_ eq "--help";
-  @dxt = @dxt1		when $_ eq "dxt1";
-  @dxt = @dxt2		when $_ eq "dxt2";
+  $dxtNum = 1		when $_ eq "dxt1";
+  $dxtNum = 2		when $_ eq "dxt2";
   $monitoring = 1	when $_ eq "--monitoringRO2";
   default {die $Usage}
 }
+open my $handle1, '<', "./dxt$dxtNum"."Cells.txt" or die "Can't open the file ./dxt$dxtNum"."Cells.txt: $!";
+my @cellmap;
+while (<$handle1>) {chomp; push @cellmap, split(/ +/)} #Vl.avoid \n on last field
+close $handle1;
 undef my $timeout;
 say "started $0 rev.", REV;
-my $exp = Expect->new("telnet", @dxt);
+my $exp = Expect->new("telnet", @{$dxt{$dxtNum}}); #Vl. @{$aref}
 # $exp->send("\r\n\r\n");
 say "manual_stty: ", $exp->manual_stty;
 $exp->log_stdout(0);
@@ -36,6 +42,16 @@ my $get1 = $exp->before;
 # say "Vladi DXT-TBS CONNECTION BREAK list:\n", $get1;
 my @dtcbIdx; #Vl.(np++)dtcb Indexes array.
 push @dtcbIdx, $1 while $get1 =~ /TBS-(\d+)/g;
+my @dtcbIdxAlias;
+for (my $i = 0; $i < @dtcbIdx; $i++) {
+  for (my $j = 1; $j < @cellmap; $j += 2) {
+    if ($cellmap[$j] == $dtcbIdx[$i]) {
+	  push @dtcbIdxAlias, $dtcbIdx[$i];
+	  push @dtcbIdxAlias, $cellmap[$j - 1];
+	  last
+	}
+  }
+}
 $exp->send("ZAHO::NR=2692;\r"); #Vl.get INCORRECT WORKING STATE (iws) alarms
 my @iws; #Vl.(np++)iws array.
 $exp->expect(10, 'EXECUTED');
@@ -45,5 +61,6 @@ push @iws, $1 while $get1 =~ /ALARM\s+(?!HIS)(\S+)/g; #Vl.(?!HIS) is not capture
 $exp->send("Z;\r");
 $exp->send("Z;\r"); #should exit here..
 say "dtcbIdx array: @dtcbIdx";
+say "dtcbIdxAlias array: @dtcbIdxAlias";
 say "iws array: @iws"
 # print "dtcbIdx array: @dtcbIdx"
