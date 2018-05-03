26880b5d4bd2566b46f8ea0437b1f5fcbe434531
Vladimir Manolescu <mvmanol@yahoo.com>
2018-05-03 18:08:28 +0300

    rev.2
    batwings@VladiLaptopWXP /home/batwings/projects/perl/V4_DXTtelnet
    $ perl -X ./V4.pl dxt2
    ..started ./V4.pl rev.2, 5:27 PM Thursday, May 03, 2018
    started ./V4.pl rev.2, 5:27 PM Thursday, May 03, 2018
    manual_stty: 0
    dtcbIdx array: 66 35 84
    iws array: TBC-66-0 CLS-0 TDSC-72-0 TDSC-75-0 TBC-84-0 TTRX-84-0 TDSC-1-0 TDSC-62-0
    batwings@VladiLaptopWXP /home/batwings/projects/perl/V4_DXTtelnet
    $ git gui

--- V4.pl
+++ V4.pl
@@ -2,12 +2,24 @@
 ##Vl.8:30 PM Wednesday, May 02, 2018: $ perl -c V4.pl; $ perl ./V4.pl; RO2 (elements) recovering code..
 use strict;
 use warnings;
-use constant REV => 1; #Vl.8:51 PM Wednesday, May 02, 2018
+use constant REV => "2, 5:27 PM Thursday, May 03, 2018";
 use Expect;
 use v5.14;
 my @dxt1 = ("10.1.153.4", 5001); #Vl.work both "5001" and (int) 5001
 my @dxt2 = ("10.1.153.4", 5002);
-my @dxt = @dxt1;
+undef my @dxt;
+undef my $monitoring;
+my $Usage = "Usage:\n$0 [--help]\n$0 dxt1|dxt2\n$0 --monitoringRO2\n";
+# say "nr params: ", $#ARGV + 1, " @ARGV";
+say "..started $0 rev.", REV;
+die $Usage if (not @ARGV or @ARGV > 1);
+for ($ARGV[$#ARGV]){
+  die $Usage 		when $_ eq "--help";
+  @dxt = @dxt1		when $_ eq "dxt1";
+  @dxt = @dxt2		when $_ eq "dxt2";
+  $monitoring = 1	when $_ eq "--monitoringRO2";
+  default {die $Usage}
+}
 undef my $timeout;
 say "started $0 rev.", REV;
 my $exp = Expect->new("telnet", @dxt);
